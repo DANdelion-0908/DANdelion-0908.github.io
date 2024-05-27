c42be4b0ad21483a32f364e75239213cada5e095
DANdelion <pen22217@uvg.edu.gt>
2024-05-26 19:00:39 -0600

    Add calculator's buttons

---
+++ app/buttons/Button.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Button.module.css';
+
+const Button = ({ symbol }) => {
+  return (
+    <div className={`${styles.Button} ${symbol === 0 ? styles.ZeroButton : ''} ${symbol === "=" ? styles.EqualButton : ''}`}>
+      {symbol}
+    </div>
+  );
+}
+
+export default Button;
